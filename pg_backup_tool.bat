1242edc293e88420fbd780589efec5824457e179
Israel Cordeiro <48436136+icordeiro@users.noreply.github.com>
2024-10-10 09:31:34 -0300

    Update and rename backup.bat to pg_backup_tool.bat
    Implementation of the feature to restart, start or stop postgres services

--- pg_backup_tool.bat
+++ pg_backup_tool.bat
@@ -9,6 +9,7 @@ set DB_USER=postgres
 set DB_NAME=vr
 set BACKUP_DIR=C:\bkp
 set LOG_FILE=C:\log.log
+set PG_SERVICE_NAME=postgresql-x64-13  REM Nome do serviço PostgreSQL (ajuste conforme a versão)
 
 REM Função para exportar o banco de dados
 :export_db
@@ -16,14 +17,33 @@ echo Iniciando exportação do banco de dados...
 pg_dump.exe -h %DB_HOST% -p %DB_PORT% -U %DB_USER% -v -Fc -f %BACKUP_DIR%\%DB_NAME%_backup.backup %DB_NAME%
 echo Exportação concluída com sucesso! >> %LOG_FILE%
 echo Exportação concluída com sucesso! Veja os detalhes no arquivo de log: %LOG_FILE%
-goto end
+goto menu
 
 REM Função para importar o banco de dados
 :import_db
 echo Iniciando importação do banco de dados...
 pg_restore.exe -h %DB_HOST% -p %DB_PORT% -U %DB_USER% -v -e -d %DB_NAME% -Fc %BACKUP_DIR%\%DB_NAME%_backup.backup >> %LOG_FILE%
 echo Importação concluída com sucesso! Veja os detalhes no arquivo de log: %LOG_FILE%
-goto end
+goto menu
+
+REM Função para iniciar o serviço do PostgreSQL
+:start_pg
+echo Iniciando o serviço PostgreSQL...
+net start %PG_SERVICE_NAME%
+goto menu
+
+REM Função para reiniciar o serviço do PostgreSQL
+:restart_pg
+echo Reiniciando o serviço PostgreSQL...
+net stop %PG_SERVICE_NAME%
+net start %PG_SERVICE_NAME%
+goto menu
+
+REM Função para parar o serviço do PostgreSQL
+:stop_pg
+echo Parando o serviço PostgreSQL...
+net stop %PG_SERVICE_NAME%
+goto menu
 
 REM Menu de seleção
 :menu
@@ -32,12 +52,18 @@ echo      FERRAMENTA DE BACKUP
 echo ===============================
 echo [1] Exportar Backup do Banco
 echo [2] Importar Backup no Banco
+echo [3] Iniciar Servico PostgreSQL
+echo [4] Reiniciar Servico PostgreSQL
+echo [5] Parar Servico PostgreSQL
 echo [0] Sair
 echo ===============================
 set /p choice="Escolha uma opcao: "
 
 if "%choice%"=="1" goto export_db
 if "%choice%"=="2" goto import_db
+if "%choice%"=="3" goto start_pg
+if "%choice%"=="4" goto restart_pg
+if "%choice%"=="5" goto stop_pg
 if "%choice%"=="0" goto end
 
 REM Se a opção for inválida
